537d03aa2ad444f8235e39310d552a8266ed6cba
MrMDWatson <mrmdwatson@gmail.com>
2024-03-09 13:55:03 -0500

    github auth

--- auth.js
+++ auth.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require("bcrypt");
 const { ObjectID } = require('mongodb');
 
@@ -31,4 +33,45 @@ module.exports = function (app, myDataBase) {
       console.log(err);
     }
   }));
+
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+    async (accessToken, refreshToken, profile, cb) => {
+      console.log(profile);
+      //Database logic here with callback containing your user object
+      try {
+        console.log(`User ${username} attempted to log in.`);
+        const user = await myDataBase.findOneAndUpdate(
+          { id: profile.id },
+          {
+            $setOnInsert: {
+              id: profile.id,
+              username: profile.username,
+              name: profile.displayName || 'John Doe',
+              photo: profile.photos[0].value || '',
+              email: Array.isArray(profile.emails)
+                ? profile.emails[0].value
+                : 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || ''
+            },
+            $set: {
+              last_login: new Date()
+            },
+            $inc: {
+              login_count: 1
+            }
+          },
+          { upsert: true, new: true });
+        if (!user) {return done(null, false);}  
+        return cb(null, user);
+      } catch (err) {
+        console.log(err);
+      }
+
+    }
+  ));
 }
--- routes.js
+++ routes.js
@@ -1,23 +1,27 @@
 const passport = require("passport");
 const bcrypt = require("bcrypt");
 
-
-
 module.exports = function (app, myDataBase) {
-  
-  // Be sure to change the title
+
   app.route('/').get((req, res) => {
     // Change the response to render the Pug template
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
-      showRegistration: true
+      showRegistration: true,
+      showSocialAuth: true
     });
   });
   
   app.route("/login").post(passport.authenticate("local", { failureRedirect: "/" }), (req, res) => {
     res.redirect("/profile");
+  })
+
+  app.route("/auth/github").get(passport.authenticate("local"));
+
+  app.route("/auth/github/callback").get(passport.authenticate("local", { failureRedirect: "/" }), (req, res) => {
+    res.redirect("./profile");
   })
 
   app.route("/profile").get(ensureAuthenticated, (req, res) => {
@@ -37,7 +41,6 @@ module.exports = function (app, myDataBase) {
     try {
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(req.body.password, salt);
-      
       const user = await myDataBase.findOne({ username: req.body.username });
       if (user) {
         res.redirect("/")
